171aa60c0c72df708cd999c3f9f730c2dacf932d
Marcin Kozubek <mkozubek@protonmail.com>
2020-01-25 16:02:14 +0100

    Added Competition Organizer
    Added simple app that shows the Code First approach of connecting .NET app to a database using Entity Framework Core

---
+++ CompetitionOrganizer/CompetitionOrganizer/CompetitionOrganizer/Data/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompetitionOrganizer.Data.Models
+{
+    class Match
+    {
+        public int MatchId { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CompetitionId { get; set; }
+        public Competition Competition { get; set; }
+    }
+}
